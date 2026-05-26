f9d1c8affe626a8d02e9f2b870f5f59a43f180ec
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-26 17:24:33 -0300

    feat: create CalculadoraController with index, show and store methods

---
+++ app/Http/Controllers/CalculadoraController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Calculo;
+
+class CalculadoraController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function show(string $tipo)
+    {
+
+    }
+
+    public function store(string $tipo, Request $request)
+    {
+
+    }
+}
