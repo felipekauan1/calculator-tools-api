d1e44c14ce1b175f03940fe5739d31b530c2dd7c
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-26 17:27:50 -0300

    feat: add calculadora routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\CalculadoraController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/calculadoras', [CalculadoraController::class, 'index']);
+Route::get('/calculadoras/{tipo}', [CalculadoraController::class, 'show']);
+Route::post('/calculadoras/{tipo}', [CalculadoraController::class, 'store']);
